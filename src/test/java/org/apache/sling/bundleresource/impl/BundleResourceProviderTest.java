773d4f0996da6d0dcf96d87875cdab33cbcf9b03
Michał Cukierman <michal.cukierman@ds.pl>
2022-09-16 12:08:50 +0200

    [WS-1989] Documentation, changing license

--- src/test/java/org/apache/sling/bundleresource/impl/BundleResourceProviderTest.java
+++ src/test/java/org/apache/sling/bundleresource/impl/BundleResourceProviderTest.java
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 package org.apache.sling.bundleresource.impl;
 
 import static org.junit.Assert.assertEquals;
@@ -34,7 +16,6 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.StreamSupport;
 
 import javax.json.Json;
 import javax.json.stream.JsonGenerator;
@@ -58,7 +39,6 @@ public class BundleResourceProviderTest {
 
         return bundle;
     }
-
 
     void addContent(Bundle bundle, String path, Map<String, Object> content) throws IOException {
         final URL url = new URL("resource:" + path);
@@ -419,7 +399,6 @@ public class BundleResourceProviderTest {
 
         for (int i = 0; i < 10; i++) {
             assertEquals("/libs/foo/d/" + i, rsrcChildren.get(i));
-
         }
     }
 }
