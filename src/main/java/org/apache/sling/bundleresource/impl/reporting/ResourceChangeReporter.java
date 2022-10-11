18f9e4adbaf98ad7e780b525eb8a1aff38e76873
Michał Cukierman <michal.cukierman@ds.pl>
2022-10-11 13:50:15 +0200

    [WS-2032] Bugfixes, changed log levels

--- src/main/java/org/apache/sling/bundleresource/impl/reporting/ResourceChangeReporter.java
+++ src/main/java/org/apache/sling/bundleresource/impl/reporting/ResourceChangeReporter.java
@@ -5,13 +5,11 @@ import static org.apache.sling.api.resource.observation.ResourceChange.ChangeTyp
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.observation.ResourceChange;
 import org.apache.sling.bundleresource.impl.BundleResourceProvider;
 import org.apache.sling.spi.resource.provider.ObservationReporter;
-import org.apache.sling.spi.resource.provider.ObserverConfiguration;
 import org.apache.sling.spi.resource.provider.ResourceContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,29 +35,15 @@ public class ResourceChangeReporter {
 
     private int reportChange(final Resource root, final ChangeType changeType,
                              final BundleResourceProvider provider, final ObservationReporter reporter) {
-        int counter = 0;
         if (log.isDebugEnabled()) {
             log.debug("Detected change for resource {} : {}", root.getPath(), changeType);
         }
 
         final List<ResourceChange> allChanges = new ArrayList<>();
         collectResourceChanges(root, changeType, provider, allChanges);
+        reporter.reportChanges(allChanges, false);
 
-        for (final ObserverConfiguration config : reporter.getObserverConfigurations()) {
-            final List<ResourceChange> perConfigChanges = allChanges.stream()
-                    .filter(change -> config.matches(change.getPath()))
-                    .filter(change -> config.getChangeTypes().contains(change.getType()))
-                    .collect(Collectors.toList());
-            log.debug("Processing config: {}", config);
-            reporter.reportChanges(config, perConfigChanges, false);
-            counter += perConfigChanges.size();
-            if (log.isTraceEnabled()) {
-                for (ResourceChange change : perConfigChanges) {
-                    log.trace("Send change for resource {}: {} to {}", change.getPath(), change.getType(), config);
-                }
-            }
-        }
-        return counter;
+        return allChanges.size();
     }
 
     private void collectResourceChanges(final Resource resource, final ResourceChange.ChangeType changeType,
